72400dc0c4d9f29b08492c84fc4d6b450dbacbfd
MiraliVaghasiya <miralivaghasiya704@gmail.com>
2025-04-27 09:17:03 +0530

    api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -9,7 +9,7 @@ instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = token;
+      config.headers.Authorization = `Bearer ${token}`; // 🛠 Fixed here
     }
     return config;
   },
